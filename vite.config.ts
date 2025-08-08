ef4dfb1df557103b0e84573236958b04bdae4d6f
thesheepcat <releomerda@protonmail.com>
2025-08-08 22:35:02 +0200

    Added PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -21,16 +21,27 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/Kaspa-logo.png",
-            sizes: "192x192",
-            type: "image/png",
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
           },
           {
-            src: "/Kaspa-logo.png",
-            sizes: "512x512",
-            type: "image/png",
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
-        ],
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
       },
       workbox: {
         // 20 mb
